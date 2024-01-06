22d18a3dbd57fb8cf6df27dd5508e9e45c8e2fb1
Gary Granger <granger@ucar.edu>
2024-01-05 21:16:36 -0700

    port track rendering to recent selenium
    the page rendering requires serving the html and javascript files through a
    local temporary web server, but it works with both the firefox and chrome web
    driver implementations, chrome is the default.  also modify the runtime
    environment so selenium can find selenium-manager.  convert NOTES.txt to
    markdown and update the kml rendering information.

--- acTrack2kml/renderkml.py
+++ acTrack2kml/renderkml.py
@@ -4,11 +4,12 @@ from selenium import webdriver
 import time
 import sys
 import os
+import subprocess as sp
+import http.client
 import logging
 
 logger = logging.getLogger(__name__)
 
-# -77.9957, 28.9685, -74.2237, 37.1555
 
 class RenderKML(object):
 
@@ -17,6 +18,9 @@ class RenderKML(object):
         self.kmlfiles = []
         self.cwd = os.getcwd()
         self.use_firefox = False
+        self.http_port = 8008
+        self.driver = None
+        self.serverp = None
 
     def setFiles(self, kmlfiles):
         self.kmlfiles = kmlfiles[:]
@@ -34,26 +38,79 @@ class RenderKML(object):
             if self.bounds:
                 js.write('var defaultbounds="%s";\n' % (self.bounds))
 
-    def dump_image_firefox(self, image):
-        options = webdriver.FirefoxOptions()
-        args = ['-headless', '--new-instance', '-P', 'test', '--safe-mode']
-        for arg in args:
-            options.add_argument(arg)
-        driver = webdriver.Firefox(executable_path="/usr/bin/firefox",
-                                   firefox_options=options)
-        driver.set_window_size(1024, 768) # optional
-        driver.get('file://%s/flight_data.html' % (self.cwd))
-        time.sleep(10)
-        driver.save_screenshot(image) # save a screenshot to disk
-        driver.quit()
+    def start_driver(self):
+        driver = None
+        if self.use_firefox:
+            options = webdriver.FirefoxOptions()
+            options.page_load_strategy = 'normal'
+            args = ['--headless', '--new-instance']
+            for arg in args:
+                options.add_argument(arg)
+            driver = webdriver.Firefox(options=options)
+        else:
+            # https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md
+            args = ["--headless=new",
+                    "--disable-extensions", "--no-first-run",
+                    "--hide-scrollbars",
+                    "--enable-logging=stderr", "--v=2",
+                    "--disable-sync"]
+            options = webdriver.ChromeOptions()
+            options.page_load_strategy = 'normal'
+            for arg in args:
+                options.add_argument(arg)
+            driver = webdriver.Chrome(options=options)
+        self.driver = driver
 
     def dump_image(self, image):
-        driver = webdriver.PhantomJS()
-        driver.set_window_size(1024, 768) # optional
-        driver.get('file://%s/flight_data.html' % (self.cwd))
+        driver = self.driver
+        driver.set_window_size(1024, 768)
+        url = f'http://localhost:{self.http_port}/flight_data.html'
+        logger.debug("...loading page: %s", url)
+        driver.get(url)
+        # selenium WebDriver has other synchronization options, but I don't
+        # know how to tell when OSM is done rendering the tracks, so sticking
+        # with the same fixed delay.
         time.sleep(10)
-        driver.save_screenshot(image) # save a screenshot to disk
-        driver.quit()
+        logger.debug("...saving screenshot: %s", image)
+        driver.save_screenshot(image)
+
+    def quit_driver(self):
+        self.driver.quit()
+        self.driver = None
+
+    def start_server(self):
+        """
+        Start simple web server for the current directory.  This works around
+        CORS restrictions.
+        """
+        # https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
+        args = [sys.executable, '-m', 'http.server', str(self.http_port)]
+        logger.info("starting http server: %s", " ".join(args))
+        self.serverp = sp.Popen(args, shell=False)
+        client = http.client.HTTPConnection('localhost', self.http_port,
+                                            timeout=1)
+        attempts = 0
+        while attempts < 5:
+            # just in case the server quits quickly due to something like
+            # address in use, wait first before polling and connecting.
+            time.sleep(1)
+            if self.serverp.poll() is not None:
+                sys.exit(self.serverp.returncode)
+            try:
+                attempts += 1
+                client.connect()
+                break
+            except ConnectionRefusedError as ex:
+                if attempts >= 5:
+                    raise ex
+        logger.info("server connection open...")
+        client.close()
+
+    def stop_server(self):
+        logger.info("closing http server...")
+        if self.serverp:
+            self.serverp.kill()
+        self.serverp.wait(5)
 
     def main(self, args):
         logger.debug("entering main...")
@@ -74,14 +131,15 @@ class RenderKML(object):
         self.setFiles(kmlfiles)
         self.setBounds(bounds)
         self.write_js()
-        if self.use_firefox:
-            self.dump_image_firefox(image)
-        else:
-            self.dump_image(image)
+        self.start_server()
+        self.start_driver()
+        self.dump_image(image)
+        self.quit_driver()
+        self.stop_server()
         return 0
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     rkml = RenderKML()
     sys.exit(rkml.main(sys.argv[1:]))
